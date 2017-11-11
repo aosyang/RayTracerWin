535526b1d6588d966f3e941c830d2d7ec972fc82
Shiyang Ao <aosyang@hotmail.com>
2017-11-11 13:27:41 -0800

    Added random unit sphere direction function. Use vector reflect instead of while-loop when generating hemisphere direction.

--- Src/Math.cpp
+++ Src/Math.cpp
@@ -7,18 +7,19 @@
 
 namespace RMath
 {
-
-	RVec3 RandomHemisphereDirection(const RVec3& Normal)
-	{
-		while (1)
-		{
-			float t1 = 2.0f * PI * Random();
-			float t2 = acosf(1.0f - 2.0f * Random());
-			RVec3 v(sinf(t1) * sinf(t2), cosf(t1) * sinf(t2), cosf(t2));
-
-			if (v.Dot(Normal) > 0.0f)
-				return v;
-		}
-	}
-
+    
+    RVec3 RandomHemisphereDirection(const RVec3& Normal)
+    {
+        RVec3 v = RandomUnitSphereDirection();
+        
+        if (v.Dot(Normal) > 0.0f)
+        {
+            return v;
+        }
+        else
+        {
+            return v.Reflect(Normal);
+        }
+    }
+    
 }
--- Src/Math.h
+++ Src/Math.h
@@ -15,6 +15,15 @@ namespace RMath
 	{
 		return (float)rand() / RAND_MAX;
 	}
+    
+    // Generate a random direction on a unit sphere
+    inline RVec3 RandomUnitSphereDirection()
+    {
+        float t1 = 2.0f * PI * Random();
+        float t2 = acosf(1.0f - 2.0f * Random());
+        float sin_t2 = sinf(t2);
+        return RVec3(sinf(t1) * sin_t2, cosf(t1) * sin_t2, cosf(t2));
+    }
 
 	// Generate random hemisphere direction with given normal direction
 	RVec3 RandomHemisphereDirection(const RVec3& Normal);
